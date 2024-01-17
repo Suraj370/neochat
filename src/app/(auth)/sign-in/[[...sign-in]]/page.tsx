4b6ad289f76b34f2779ae63b916cbfae76692035
Suraj370 <surajpanda2077@gmail.com>
2024-01-17 21:13:59 +0530

    Added logo for the website

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
 import Link from "next/link";
+import Image from "next/image";
  
 export default function Page() {
   return (<>
   <div className="flex flex-col">
+    <div className="flex justify-center">
+      <Image src={"/images/logo.png"} alt = "neochat"  width={250} height={250}/>
+    </div>
   <SignIn />
   <div className="text-sm mt-2 ml-5 flex gap-2">
     <p>Forgot Password?</p>
